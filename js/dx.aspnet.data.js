e772ae7e51f369e7a8e32347a17ad8b0d28c0bf7
Ling Xiao <ling-xiao.chok.kx@renesas.com>
2025-10-03 11:05:55 +0800

    feat: make delete also behave like post put overrides

--- js/dx.aspnet.data.js
+++ js/dx.aspnet.data.js
@@ -270,7 +270,7 @@
                 return send("delete", true, {
                     url: deleteUrl,
                     method: options.deleteMethod || "DELETE",
-                    data: { key: serializeKey(key) }
+                    data: JSON.stringify(values)
                 });
             }
 
